db427ba5bf2bf0ba4f6656c175cf86e10a721974
kotechi <aditiya53op@gmail.com>
2025-09-29 17:00:44 +0700

    .

--- app/Models/divisi.php
+++ app/Models/divisi.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class divisi extends Model
+class Divisi extends Model
 {
     protected $table = 'divisis';
     protected $fillable = [
