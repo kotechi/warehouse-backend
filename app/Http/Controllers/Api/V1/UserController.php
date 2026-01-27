aa674884ef26baa4203e9f1feeaf7d24e2adba32
kotechi <aditiya53op@gmail.com>
2026-01-27 11:34:39 +0700

    fix user list

--- app/Http/Controllers/Api/V1/UserController.php
+++ app/Http/Controllers/Api/V1/UserController.php
@@ -13,7 +13,7 @@ class UserController extends Controller
 {
     public function index()
     {
-        $users = User::with('jabatan', 'divisi')->get();
+        $users = User::with('jabatan', 'divisi')->get()->whereIsNot('role', 'superadmin');
         return UserResource::collection($users);
     }
 
